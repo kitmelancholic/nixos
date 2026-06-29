1222231626b249c3a7b6867625f28f242830cce2
Oleksii Kostyk <oleksij.kostyk@gmail.com>
2026-06-29 16:45:33 +0000

    ninth commit

--- modules/nixos/profiles/foundryvtt.nix
+++ modules/nixos/profiles/foundryvtt.nix
@@ -6,7 +6,11 @@
   services.foundryvtt = {
     enable = true;
     minifyStaticFiles = true;
-    package = inputs.foundryvtt.packages.${pkgs.stdenv.hostPlatform.system}.foundryvtt_13;
+    package =
+      inputs.foundryvtt.packages.${pkgs.stdenv.hostPlatform.system}.foundryvtt_14.overrideAttrs
+        (_: {
+          version = "14.0.0+360";
+        });
     upnp = false;
   };
 
